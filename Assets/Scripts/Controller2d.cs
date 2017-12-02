47e6f253091f643c774dced04e32cebf82124c3f
Cameron Robinson <cameron@IMac.home>
2017-12-02 02:18:45 -0800

    Have player moving around screen, using raycasting collision detection.

---
+++ Assets/Scripts/Controller2d.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2d : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	public int horizontalRayCount = 4;
+	public int verticalRayCount = 4;
+
+	private const float skinWidth = 0.015f;
+
+	private float horrizontalRaySpacing;
+	private float verticalRaySpacing;
+
+	private new BoxCollider2D collider;
+	private RaycastOrigins raycastOrigins;
+	public CollisionInfo collisions;
+
+	void Start () 
+	{
+		collider = GetComponent<BoxCollider2D>();
+
+		CalculateRaySpacing();
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void Move(Vector3 velocity)
+	{
+		UpdateRaycastOrigins();
+		collisions.Reset();
+
+		if(velocity.x != 0)
+		{
+			HorizontalCollisions(ref velocity);
+		}
+
+		if(velocity.y != 0)
+		{
+			VerticalCollisions(ref velocity);
+		}
+
+		transform.Translate(velocity);
+	}
+
+	private void HorizontalCollisions(ref Vector3 velocity)
+	{
+		float directionX = Mathf.Sign(velocity.x);
+		float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+		for(int i = 0; i < horizontalRayCount; i++)
+		{
+			Vector2 rayOrigin = (directionX == -1? raycastOrigins.bottomLeft : raycastOrigins.bottomRight);
+			rayOrigin += Vector2.up * (horrizontalRaySpacing * i);
+
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+			if(hit)
+			{
+				velocity.x = (hit.distance - skinWidth) * directionX;
+				rayLength = hit.distance;
+
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
+			}
+		}
+	}
+
+	private void VerticalCollisions(ref Vector3 velocity)
+	{
+		float directionY = Mathf.Sign(velocity.y);
+		float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+		for(int i = 0; i < verticalRayCount; i++)
+		{
+			Vector2 rayOrigin = (directionY == -1? raycastOrigins.bottomLeft : raycastOrigins.topLeft);
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+			if(hit)
+			{
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLength = hit.distance;
+
+				collisions.below = directionY == -1;
+				collisions.above = directionY == 1;
+			}
+		}
+	}
+
+	private void UpdateRaycastOrigins() 
+	{
+		Bounds bounds = CalcRayBounds();
+
+		raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+		raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+		raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+		raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+	}
+
+	private void CalculateRaySpacing() 
+	{
+		Bounds bounds = CalcRayBounds();
+
+		horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+		verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+		horrizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+	}
+
+	private Bounds CalcRayBounds() 
+	{
+		Bounds bounds = collider.bounds;
+		bounds.Expand(skinWidth * -2);
+
+		return bounds;
+	}
+
+	struct RaycastOrigins 
+	{
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+
+	public struct CollisionInfo
+	{
+		public bool above, below;
+		public bool left, right;
+
+		public void Reset() 
+		{
+			above = below = false;
+			left = right = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2d))]
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private float jumpHeight = 4;
+	[SerializeField]
+	private float timeToJumpApex = 0.4f;
+	[SerializeField]
+	private float moveSpeed = .6f;
+
+	private float accelerationTimeAirborne = .2f;
+	private float accelerationTimeGrounded = .1f;
+
+	private float velocityXsmoothing;
+
+	private float gravity;
+	private float jumpVelocity;
+
+	private Vector2 velocity;
+
+	private Controller2d controller;
+
+	// Use this for initialization
+	void Start () {
+	
+		controller = GetComponent<Controller2d>();
+		CalculateGravityAndJumpVel();
+	}
+
+	private void CalculateGravityAndJumpVel()
+	{
+		gravity = -1 * (2 * jumpHeight) / (Mathf.Pow(timeToJumpApex, 2));
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		Debug.Log("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(controller.collisions.above || controller.collisions.below)
+		{
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		if(Input.GetButtonDown("Jump") && controller.collisions.below)
+		{
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXsmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity);
+	}
+}
