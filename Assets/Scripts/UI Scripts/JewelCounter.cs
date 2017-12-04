3cfffcdcb348046c7843da40a0dd7a84e3ef67c7
Cameron Robinson <cameron@IMac.home>
2017-12-04 09:02:53 -0800

    Updated game over screen text. Implemented InGame UI assets.

--- Assets/Scripts/UI Scripts/JewelCounter.cs
+++ Assets/Scripts/UI Scripts/JewelCounter.cs
@@ -8,9 +8,30 @@ public class JewelCounter : MonoBehaviour {
 	[SerializeField]
 	private PlayerStats playerStats;
 	[SerializeField]
+	private GameSettings gameSettings;
+
+	[SerializeField]
 	private TextMeshProUGUI jewelCounter;
-	
+	[SerializeField]
+	private TextMeshProUGUI timer;
+
 	void Update () {
+
+		float minutes = Mathf.Floor(gameSettings.gameTime / 60); 
+		float seconds = Mathf.RoundToInt(gameSettings.gameTime % 60);
+
+		string sMinutes = minutes.ToString();
+		string sSeconds = Mathf.RoundToInt(seconds).ToString();
+		if(minutes < 10) 
+		{ 
+			sMinutes = "0" + sMinutes; 
+		} 
+		if(seconds < 10) 
+		{ 
+			sSeconds = "0" + sSeconds;
+		} 
+
+		timer.text = sMinutes + ":" + sSeconds;
 
 		//TODO: make this only change when the value changes instead of every frame.
 		jewelCounter.text = playerStats.JewelValue.ToString();
